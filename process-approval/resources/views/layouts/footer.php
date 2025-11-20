388c475537f10b55ed5b5069da17122d89a8fff5
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-11-20 16:21:34 +0200

    add chnages t oocdebase

---
+++ process-approval/resources/views/layouts/footer.php
@@ -0,0 +1,12 @@
+
+
+<footer>
+    <div class="footer-content">
+        <p>&copy; {{ date('Y') }} FlowCheck @2025. All rights reserved.</p>
+        <div class="footer-links">
+            <a href="{{ url('/privacy') }}">Privacy Policy</a>
+            <a href="{{ url('/terms') }}">Terms of Service</a>
+        </div>
+    </div>
+</footer>
+
---
+++ process-approval/resources/views/layouts/header.php
@@ -0,0 +1,12 @@
+<header>
+    <nav>
+        <div class="logo">
+            <a href="{{ url('/') }}">FlowCheck</a>
+        </div>
+        <ul class="nav-links">
+            <li><a href="{{ url('/') }}">Home</a></li>
+            <li><a href="{{ url('/about') }}">About</a></li>
+            <li><a href="{{ url('/contact') }}">Contact</a></li>
+        </ul>
+    </nav>
+</header>
